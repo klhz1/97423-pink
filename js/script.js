c06a7712634f1729b2cee36ce52cd2804003df7e
klhz1 <vahterovs@gmail.com>
2015-10-06 14:10:48 +0200

    task4

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var menuOpen = document.querySelector(".navigation__menu-open");
+    var menu = document.querySelector(".navigation__menu");
+    var menuClose = document.querySelector(".navigation__menu-close");
+
+    menuOpen.addEventListener("click", function (event) {
+      event.preventDefault();
+      menu.classList.add("navigation__menu--show");
+    });
+
+
+    menuClose.addEventListener("click", function (event) {
+      event.preventDefault();
+      menu.classList.remove("navigation__menu--show");
+    });
