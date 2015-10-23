836a6b6bff40ebfaad8f8b3422c6b740511850dc
klhz1 <vahterovs@gmail.com>
2015-10-23 17:54:00 +0200

    сделал автоматизацию

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,25 +6,83 @@ module.exports = function(grunt) {
   var config = {
     pkg: grunt.file.readJSON('package.json'),
 
+    clean: {
+      build: ["build"]
+    },
+    copy: {
+      build: {
+        files: [{
+          expand: true,
+          cwd: "source",
+          src: [
+          "img/**",
+          "js/**",
+          "index.html"
+          ],
+          dest: "build"
+        }]
+      }
+    },
     sass: {
       style: {
         files: {
-          'css/style.css': 'sass/style.scss'
+          'build/css/style.css': 'source/sass/style.scss'
         }
       }
     },
-
+    cmq: {
+      style: {
+        files: {
+          "build/css/style.css": ["build/css/style.css"]
+        }
+      }
+    },
     postcss: {
       options: {
         processors: [
-          require('autoprefixer')({browsers: 'last 2 versions'})
+        require('autoprefixer')({browsers: 'last 2 versions'})
         ]
       },
       style: {
-        src: 'css/*.css'
+        src: 'build/css/style.css'
       }
     },
-
+    cssmin: {
+      options: {
+        keepSpecialComments: 0,
+        report: "gzip"
+      },
+      style: {
+        files: {
+          "build/css/style.min.css": ["build/css/style.css"]
+        }
+      }
+    },
+    imagemin: {
+      images: {
+        options: {
+          optimizationlevel: 3
+        },
+        files: [{
+          expand: true,
+          src: ["build/img/**/*.{png,jpg,gif,svg}"]
+        }]
+      }
+    },
+    htmlmin: {
+      options: {
+        removeComments: true,
+        collapseWhitespace: true,
+        collapseBooleanAttributes: true,
+        caseSensitive: true,
+        keepClosingSlash: false
+      },
+      html: {
+        files: {
+          "build/index.min.html": "build/index.html"
+        }
+      }
+    },
     watch: {
       style: {
         files: ['sass/**/*.scss'],
@@ -34,10 +92,28 @@ module.exports = function(grunt) {
           livereload: true
         }
       }
-    }
+    },
+    csscomb: {
+      style: {
+        expand: true,
+        src: ["sass/**/*.scss"]
+      }
+    },
   };
+
 
   config = require('./.gosha')(grunt, config);
 
   grunt.initConfig(config);
+
+  grunt.registerTask("build", [
+    "clean",
+    "copy",
+    "sass",
+    "cmq",
+    "postcss",
+    "cssmin",
+    "imagemin",
+    "htmlmin"
+    ]);
 };
